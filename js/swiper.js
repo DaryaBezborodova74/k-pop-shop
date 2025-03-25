79b592097e645aa01fdf05d21613ca2385b3a4d1
DaryaBezborodova74 <bezborodova-d2004@mail.ru>
2025-03-25 16:44:13 +0500

    cart

--- js/swiper.js
+++ js/swiper.js
@@ -18,8 +18,27 @@ const swiperCard = new Swiper('.swiper-card', {
     // scrollbar: {
     //   el: '.swiper-scrollbar',
     // },
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    //   },
+  });
+
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+  
+  });
+  var swiper2 = new Swiper(".mySwiper2", {
+    spaceBetween: 10,
+    slidesPerView: 'auto',
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiper,
+    },
   });
