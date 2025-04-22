31d43455b73d44b1cf56813dfed4bc62ceda41ee
DaryaBezborodova74 <bezborodova-d2004@mail.ru>
2025-04-22 11:15:34 +0500

    btn_goods

--- js/filter_button.js
+++ js/filter_button.js
@@ -1,7 +1,13 @@
-const btnShowFilterPanel = document.getElementById("#btn-show-filter-panel-");
+const btnShowFilterPanel = document.getElementById("btn-show-filter-panel");
 const filterPanel = document.querySelector(".catalog__filter");
 
 btnShowFilterPanel.addEventListener("click", () => {
     filterPanel.classList.add("show-filter-panel");
     
 })
+
+// const btnCloseFilterPanel = document.getElementById("btn-close-filter-panel");
+// btnCloseFilterPanel.addEventListener("click", () => {
+//     filterPanel.classList.remove("show-filter-panel");
+    
+// })
